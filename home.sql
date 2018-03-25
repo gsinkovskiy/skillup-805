e1229ce55da0325c58177201d85e0f451ea91c13
Hlib Synkovskyi <gleb.sinkovskiy@gmail.com>
2018-03-25 13:07:42 +0300

    lesson 13

---
+++ home.sql
@@ -0,0 +1,5 @@
+# Поставьте Диме зарплату в 1000$ и возраст 20 лет.
+UPDATE workers SET salary = 1000, age = 20 WHERE name = 'Дима';
+
+# sdfasdfsadgdf
+SELECT 1
--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 
-session_start();
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=gleb;charset=utf8', 'gleb', 'gleb');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Подключение успешно.';
+} catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
 
-$_SESSION['test'] = 'Тестовая запись в сессию';
+$sql = "SELECT id, username FROM slim_users ORDER BY username DESC";
+$result = $pdo->query($sql);
 
-?>
-<a href="session.php">перейти ко второй странице</a>
+while ($row = $result->fetch(PDO::FETCH_OBJ)) {
+    var_dump($row);
+//    echo "<p>".$row['id']." | ".$row['username']."</p>";
+}
+
+$users = ['user13', 'user14', 'user15'];
+$sql = "INSERT INTO slim_users (username) VALUES (:user)";
+$query = $pdo->prepare($sql);
+
+foreach ($users as $user) {
+    $query->bindParam('user', $user);
+    $query->execute();
+    $lastId = $pdo->lastInsertId();
+    echo 'Пользователь '.$user.' вставлен с id = '.$lastId.'<br>';
+}
+
